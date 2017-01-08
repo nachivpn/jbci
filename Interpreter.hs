b4fab141650883368434a7a6f51a10cf6ec30afd
nachivpn <nachivpn@gmail.com>
2017-01-08 16:32:16 +0100

    adds comments and cleans up

--- Interpreter.hs
+++ Interpreter.hs
@@ -6,6 +6,7 @@ import Data.Char
 import Data.Maybe
 import qualified Data.Map as Map
 
+-- State contains the program counter, variable storage and the stack
 type State = (Int, Storage, Stack Integer)
 
 data Instruction = BIPUSH Integer | IADD | IMUL | ILOAD Int
@@ -13,6 +14,9 @@ data Instruction = BIPUSH Integer | IADD | IMUL | ILOAD Int
                  | LABEL String
                  deriving (Show, Eq, Ord)
 
+-- eval evaluates an instruction given the state of the machine
+-- and a label environment containing label and corresponding locations in the program
+-- the results of the evaluation are reflected in the resulting state
 eval :: Instruction -> State -> Map.Map Instruction Int -> State
 eval (BIPUSH i) (p,v,s) le = (p + 1, v, push i s)
 eval (IADD) (p,v,s) le = let (y,s') = pop s
@@ -24,10 +28,8 @@ eval (IMUL) (p,v,s) le = let (y,s') = pop s
 eval (ILOAD i) (p,v,s) le = (p + 1, v, push (read $ get v i) s)
 eval (ISTORE i) (p,v,s) le = let (x,s') = pop s
                               in (p+1, store v i 1 (show x), s')
-
 eval (GOTO l) (p,v,s) le = let p' = lookupLabelLoc l le
                             in (p', v, s) 
-
 eval (LABEL l) (p,v,s) le = (p + 1, v, s)
 eval (IFEQ l) (p,v,s) le = let (x,s') = pop s
                                p' = lookupLabelLoc l le
@@ -35,7 +37,8 @@ eval (IFEQ l) (p,v,s) le = let (x,s') = pop s
                                   then (p', v, s')
                                   else (p+1,v,s')
 
-                                       
+-- Parses the program, runs the interpreter
+-- prints the value remaining on top of the stack
 main :: IO()
 main = do
     code <- readFile "code.j"
@@ -54,6 +57,9 @@ interpret' is state@(pc,vars,stack) le
   | pc >= length is = state
   | otherwise = interpret' is (eval (is!!pc) state le) le
 
+-----------------------------------------------------------
+-- PARSER --
+-----------------------------------------------------------
 parse' :: String -> Maybe [Instruction]
 parse' code = maybeFst [ parse instruction i | i<-(preparse code)]
 
@@ -127,16 +133,21 @@ instruction = do
             char ':'
             return (LABEL c)
 
+------------------------------------------------------------
+-- LABEL environment functions --
+------------------------------------------------------------
 isLabel :: Instruction -> Bool
 isLabel (LABEL a) = True
 isLabel _ = False
 
 getLabelIndices = findIndices isLabel
 
+-- index labels in the given instruction list
 labelEnv :: [Instruction]-> Map.Map Instruction Int
 labelEnv is = Map.fromList [(is!!index, index) | index<-labelIndices]
     where labelIndices = getLabelIndices is
 
+-- lookup the position of a label in the program using it's name
 lookupLabelLoc :: String -> Map.Map Instruction Int ->  Int
 lookupLabelLoc l m = lookupLabelLoc' $ Map.lookup (LABEL l) m
     where 
